217bfcff51c6a858778305e961c986cd28a53bab
Bohyun Suh <bhsuhkr@gmail.com>
2019-06-06 17:40:04 -0500

    add button

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.todo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    final ArrayList<String> listItems = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void addItems(View v) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.alertdialog_view, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText edt = dialogView.findViewById(R.id.newItem);
+
+        dialogBuilder.setTitle("New Item");
+
+        dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String insertedValue = edt.getText().toString();
+                if(insertedValue.length() != 0){
+//                    listItems.set(curPosition, edt.getText().toString());
+//                    startAdapter();
+                }else{
+                    Toast.makeText(MainActivity.this, "Enter a value", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //pass
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
     }
 }
