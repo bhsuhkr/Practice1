efe809a4fba3b2f59027b7579a7a8d04f30560af
Bohyun Suh <bhsuhkr@gmail.com>
2019-06-07 00:04:38 -0500

    add new task

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -11,14 +14,42 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnRecyclerViewItemClickListener{
 
     final ArrayList<String> listItems = new ArrayList<String>();
-
+    private RecyclerView mainRecyclerView;
+    private MainRecyclerAdapter mainRecyclerAdapter;
+    private  ArrayList<MainModel> mainModelArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainRecyclerView = findViewById(R.id.my_recycler_view);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        mainRecyclerView.setLayoutManager(linearLayoutManager);
+        startAdapter();
+    }
+
+    private void startAdapter(){
+        mainModelArrayList = prepareList();
+        mainRecyclerAdapter = new MainRecyclerAdapter(this,mainModelArrayList);
+        mainRecyclerAdapter.setOnRecyclerViewItemClickListener(this);
+        mainRecyclerView.setAdapter(mainRecyclerAdapter);
+    }
+
+    private ArrayList<MainModel> prepareList() {
+        ArrayList<MainModel> mainModelList = new ArrayList<>();
+        for (int i = 0; i < listItems.size(); i++) {
+            MainModel mainModel = new MainModel();
+            mainModel.setOfferName(listItems.get(i));
+            mainModelList.add(mainModel);
+        }
+        return mainModelList;
+    }
+
+    @Override
+    public void onItemClick(final int position, View view) {
 
     }
 
@@ -30,14 +61,15 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText edt = dialogView.findViewById(R.id.newItem);
 
-        dialogBuilder.setTitle("New Item");
+        dialogBuilder.setTitle("New Task");
 
         dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 String insertedValue = edt.getText().toString();
+                Log.d("TEST", insertedValue);
                 if(insertedValue.length() != 0){
-//                    listItems.set(curPosition, edt.getText().toString());
-//                    startAdapter();
+                    listItems.add(insertedValue);
+                    startAdapter();
                 }else{
                     Toast.makeText(MainActivity.this, "Enter a value", Toast.LENGTH_SHORT).show();
                 }
---
+++ app/src/main/java/com/example/todo/OnRecyclerViewItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.todo;
+
+import android.view.View;
+
+public interface OnRecyclerViewItemClickListener {
+    void onItemClick(int position, View view);
+}
+
